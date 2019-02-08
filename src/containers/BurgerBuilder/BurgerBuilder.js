164da8cab697965c978cdcac8656b79f328a0798
pedrobarata <pedrofelipebarata@gmail.com>
2019-02-08 15:07:37 -0200

    feat(8.149): Starting Implementation of The Burger Builder Container

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <>
+        <div>Burger</div>
+        <div>BurgerControls</div>
+      </>
+    );
+  }
+}
+
+export default BurgerBuilder;
