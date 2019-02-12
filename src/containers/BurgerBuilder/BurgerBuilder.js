43b45d66e36f7d3eef0eed1e620f84a4caaec786
pedrobarata <pedrofelipebarata@gmail.com>
2019-02-12 17:54:23 -0200

    feat(8.157): Connecting State to Build Controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
-import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICE = {
+  salad: 0.5,
+  cheese: 0.4,
+  bacon: 0.7,
+  meat: 1.3
+};
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -8,13 +15,36 @@ class BurgerBuilder extends Component {
       bacon: 0,
       meat: 0,
       cheese: 0
-    }
+    },
+    totalPrice: 4
   };
+
+  addIngredientHandler = type => {
+    /* const oldCount = this.state.ingredients[type];
+      const updatedCount = oldCount +1; 
+      const updatedIngredients = {
+        ...this.state.ingredients
+      }
+      updatedIngredients[type] = updatedCount;
+      const priceAddition = INGREDIENT_PRICE[type];
+      const oldPrice = this.state.totalPrice
+      const newPrice = oldPrice + priceAddition;
+      this.setState({totalPrice: newPrice, ingredients: updatedIngredients})*/
+
+    const newIngredients = {...this.state.ingredients};
+    newIngredients[type] = newIngredients[type] + 1;
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + INGREDIENT_PRICE[type];
+    this.setState({ingredients: newIngredients, totalPrice: newPrice});
+  };
+
+  removeIngredientHandler = type => {};
+
   render() {
     return (
       <>
-        <Burger ingredients={this.state.ingredients}/>
-        <BuildControls />
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls addIngredient={this.addIngredientHandler} />
       </>
     );
   }
