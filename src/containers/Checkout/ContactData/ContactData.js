c1c273f38aa481bfb0acaf5aa3f66aae2c80a8ce
pedrobarata <pedrofelipebarata@gmail.com>
2019-02-28 20:05:59 -0300

    feat(13.261): Setting Up a JS Config for the Form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,38 +1,71 @@
 import React, { Component } from "react";
 import Button from "../../../components/UI/Button/Button";
 import Spinner from "../../../components/UI/Spinner/Spinner";
-import Input from '../../../components/UI/Input/Input';
+import Input from "../../../components/UI/Input/Input";
 import classes from "./ContactData.css";
 import axios from "../../../axios-orders";
 class ContactData extends Component {
   state = {
-    name: "",
-    email: "",
-    address: {
-      street: "",
-      postalCode: ""
+    name: {
+      elementType: "input",
+      elementConfig: {
+        type: "text",
+        placeholder: "Your Name"
+      },
+      value: ""
+    },
+    email: {
+      elementType: "input",
+      elementConfig: {
+        type: "text",
+        placeholder: "Your E-Mail"
+      },
+      value: ""
+    },
+    street: {
+      elementType: "input",
+      elementConfig: {
+        type: "text",
+        placeholder: "Street"
+      },
+      value: ""
+    },
+    zipCode: {
+      elementType: "input",
+      elementConfig: {
+        type: "text",
+        placeholder: "ZIP Code"
+      },
+      value: ""
+    },
+    country: {
+      elementType: "input",
+      elementConfig: {
+        type: "text",
+        placeholder: "Your Country"
+      },
+      value: ""
+    },
+    deliveryMethod: {
+      elementType: "select",
+      elementConfig: {
+        options: [
+          { value: "fastest", displayValue: "Fastest" },
+          { value: "cheapest", displayValue: "Cheapest" }
+        ]
+      }
     },
     loading: false
   };
 
   orderHandler = event => {
-      console.log(this.props);
-      
+    console.log(this.props);
+
     this.setState({ loading: true });
     event.preventDefault();
     const order = {
       ingredients: this.props.ingredients,
-      price: +this.props.price,
-      customer: {
-        name: "Pedro",
-        email: "teste@mail.com",
-        address: {
-          street: "testStreet",
-          number: "12345",
-          country: "Brasil"
-        }
-      },
-      deliveryMethod: "cheapest"
+      price: +this.props.price
     };
     axios
       .post("/orders.json", order)
