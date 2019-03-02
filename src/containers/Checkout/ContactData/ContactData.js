9d7ea8a95a07e0506b63f89eeaaa40cf1f7348e7
pedrobarata <pedrofelipebarata@gmail.com>
2019-03-02 12:55:47 -0300

    feat(13.266): Adding Custom Form Validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -13,7 +13,11 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Your Name"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
       email: {
         elementType: "input",
@@ -21,7 +25,11 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Your E-Mail"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
       street: {
         elementType: "input",
@@ -29,7 +37,11 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Street"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
       zipCode: {
         elementType: "input",
@@ -37,7 +49,13 @@ class ContactData extends Component {
           type: "text",
           placeholder: "ZIP Code"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false
       },
       country: {
         elementType: "input",
@@ -45,7 +63,11 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Your Country"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
       deliveryMethod: {
         elementType: "select",
@@ -60,12 +82,32 @@ class ContactData extends Component {
     loading: false
   };
 
+  checkValidity = (value, rules) => {
+    let isValid = false;
+    if (rules.required) {
+      isValid = value.trim() !== "";
+    }
+
+    if(rules.minLength) {
+      isValid = value.length >= rules.minLength;
+    }
+
+    if(rules.maxLength) {
+      isValid = value.length <= rules.maxLength;
+      
+    }
+
+    return isValid;
+  };
+
   orderHandler = event => {
     this.setState({ loading: true });
     event.preventDefault();
     const orderData = {};
     for (let formDataIdenfier in this.state.orderForm) {
-      orderData[formDataIdenfier] = this.state.orderForm[formDataIdenfier].value;
+      orderData[formDataIdenfier] = this.state.orderForm[
+        formDataIdenfier
+      ].value;
     }
 
     const order = {
@@ -90,7 +132,9 @@ class ContactData extends Component {
     const updatedOrderForm = { ...this.state.orderForm };
     const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     updatedOrderForm[inputIdentifier] = updatedFormElement;
+    console.log(updatedFormElement);
     this.setState({ orderForm: updatedOrderForm });
   };
 
