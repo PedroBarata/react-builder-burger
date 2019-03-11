ef5294572594e3526b11c38ca8eac8346bac0a08
pedrobarata <pedrofelipebarata@gmail.com>
2019-03-11 10:58:27 -0300

    feat(15.299): Finishing the Reducer for Ingredients

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,13 +1,38 @@
 import * as actionTypes from "./actions";
 
 const initialState = {
-    ingredients: null,
+    ingredients: {
+        salad: 0,
+        meat: 0,
+        cheese: 0,
+        egg: 0,
+        bacon: 0
+    },
     price: 4
 }
 
 
 const reducer = (state = initialState, action) => {
-    return state;
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                }
+            };
+        case actionTypes.REMOVE_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+                }
+            };
+        default:
+            return state;
+    }
 }
 
 export default reducer;
