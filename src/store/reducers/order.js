79d8b8975c7f689dd4350d18bc7347aa7ff4a71e
pedrobarata <pedrofelipebarata@gmail.com>
2019-04-08 11:00:42 -0300

    feat(17.338): Fetching Orders (via Redux)

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -8,15 +8,15 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-      case actionTypes.PURCHASE_BURGER_INIT:
+    case actionTypes.PURCHASE_BURGER_INIT:
       return {
-          ...state,
-          purchased: false
+        ...state,
+        purchased: false
       }
     case actionTypes.PURCHASE_BURGER_START:
       return {
-          ...state,
-          loading: true
+        ...state,
+        loading: true
       };
     case actionTypes.PURCHASE_BURGER_SUCCESS:
       const newOrder = {
@@ -34,6 +34,22 @@ const reducer = (state = initialState, action) => {
         ...state,
         loading: false
       };
+    case actionTypes.FETCH_ORDERS_START:
+      return {
+        ...state,
+        loading: true
+      }
+    case actionTypes.FETCH_ORDERS_SUCCESS:
+      return {
+        ...state,
+        orders: action.orders,
+        loading: false
+      }
+    case actionTypes.FETCH_ORDERS_FAIL:
+      return {
+        ...state,
+        loading: false
+      }
     default:
       return state;
   }
