9862fa1f9337340bafe112d48a69cdac71d23b2b
pedrobarata <pedrofelipebarata@gmail.com>
2019-04-10 12:54:06 -0300

    feat(17.341): Refactoring Reducers

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,61 +1,56 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updatedObject } from "../utility";
 
 const initialState = {
-    ingredients: null,
-    totalPrice: 4,
-    error: false
-}
+  ingredients: null,
+  totalPrice: 4,
+  error: false
+};
 
 const INGREDIENT_PRICE = {
-    salad: 0.5,
-    cheese: 0.4,
-    bacon: 0.7,
-    meat: 1.3,
-    egg: 0.5
-  };
-
+  salad: 0.5,
+  cheese: 0.4,
+  bacon: 0.7,
+  meat: 1.3,
+  egg: 0.5
+};
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case actionTypes.ADD_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                },
-                totalPrice: state.totalPrice + INGREDIENT_PRICE[action.ingredientName]
-            };
-        case actionTypes.REMOVE_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-                },
-                totalPrice: state.totalPrice - INGREDIENT_PRICE[action.ingredientName]
-            };
-            case actionTypes.SET_INGREDIENTS:
-            return {
-                ...state,
-                ingredients: {
-                    salad: action.ingredients.salad,
-                    bacon: action.ingredients.bacon,
-                    egg: action.ingredients.egg,
-                    cheese: action.ingredients.cheese,
-                    meat: action.ingredients.meat
-                },
-                totalPrice: 4,
-                error: false
-            }
-            case actionTypes.FETCH_INGREDIENTS_FAILED:
-            return {
-                ...state,
-                error: true
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENT:
+      const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 };
+      const updatedIngredients = updatedObject(state.ingredients, updatedIngredient);
+      const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice + INGREDIENT_PRICE[action.ingredientName]
+      }
+      return updatedObject(state, updatedState);
+    case actionTypes.REMOVE_INGREDIENT:
+      const updatedIngr = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 };
+      const updatedIngrs = updatedObject(state.ingredients, updatedIngr);
+      const updatedSt = {
+        ingredients: updatedIngrs,
+        totalPrice: state.totalPrice + INGREDIENT_PRICE[action.ingredientName]
+      }
+      return updatedObject(state, updatedSt);
+    case actionTypes.SET_INGREDIENTS:
+      return updatedObject(state, {
+        ingredients: {
+          salad: action.ingredients.salad,
+          bacon: action.ingredients.bacon,
+          egg: action.ingredients.egg,
+          cheese: action.ingredients.cheese,
+          meat: action.ingredients.meat
+        },
+        totalPrice: 4,
+        error: false
+      });
+
+    case actionTypes.FETCH_INGREDIENTS_FAILED:
+      return updatedObject(state, { error: true });
+    default:
+      return state;
+  }
+};
 
 export default reducer;
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,71 +1,45 @@
-import * as actionTypes from "../actions/actionTypes";
+import * as actionTypes from '../actions/actionTypes';
+import { updatedObject } from '../utility';
 
 const initialState = {
   orders: [],
   loading: false,
-  purchased: false
+  purchased: false,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.PURCHASE_BURGER_INIT:
-      return {
-        ...state,
-        purchased: false
-      }
+      return updatedObject(state, { purchased: false });
     case actionTypes.PURCHASE_BURGER_START:
-      return {
-        ...state,
-        loading: true
-      };
+      return updatedObject(state, { loading: true });
     case actionTypes.PURCHASE_BURGER_SUCCESS:
-      const newOrder = {
-        ...action.orderData,
-        id: action.orderId
-      };
-      return {
-        ...state,
+      const newOrder = updatedObject(action.orderData, { id: action.orderId });
+      return updatedObject(state, {
         loading: false,
         purchased: true,
-        orders: state.orders.concat(newOrder)
-      };
+        orders: state.orders.concat(newOrder),
+      });
     case actionTypes.PURCHASE_BURGER_FAIL:
-      return {
-        ...state,
-        loading: false
-      };
+      return updatedObject(state, { loading: false });
     case actionTypes.FETCH_ORDERS_START:
-      return {
-        ...state,
-        loading: true
-      }
+      return updatedObject(state, { loading: true });
     case actionTypes.FETCH_ORDERS_SUCCESS:
-      return {
-        ...state,
+      return updatedObject(state, {
         orders: action.orders,
-        loading: false
-      }
-    case actionTypes.FETCH_ORDERS_FAIL:
-      return {
-        ...state,
-        loading: false
-      }
-    case actionTypes.DELETE_ORDER_START:
-      return {
-        ...state,
-        loading: true
-      }
-    case actionTypes.DELETE_ORDER_SUCCESS:
-      return {
-        ...state,
         loading: false,
-        orders: state.orders.filter(order => order.id !== action.orderId)
-      }
+      });
+    case actionTypes.FETCH_ORDERS_FAIL:
+      return updatedObject(state, { loading: false });
+    case actionTypes.DELETE_ORDER_START:
+      return updatedObject(state, { loading: true });
+    case actionTypes.DELETE_ORDER_SUCCESS:
+      return updatedObject(state, {
+        loading: false,
+        orders: state.orders.filter((order) => order.id !== action.orderId),
+      });
     case actionTypes.DELETE_ORDER_FAIL:
-      return {
-        ...state,
-        loading: false
-      }
+      return updatedObject(state, { loading: false });
     default:
       return state;
   }
