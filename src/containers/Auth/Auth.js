fa498871254211cffa0f290b995b40f74bfe6136
pedrobarata <pedrofelipebarata@gmail.com>
2019-05-02 17:15:35 -0300

    feat(18.361): Import do Redirect

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,7 @@ import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 import { connect } from 'react-redux';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import { Redirect } from 'react-router-dom';
 class Auth extends Component {
   state = {
     controls: {
