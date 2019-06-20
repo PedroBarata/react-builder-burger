887ebccc0a830999b49375cdff094fea67e91726
pedrobarata <pedrofelipebarata@gmail.com>
2019-06-20 15:05:26 -0300

    feat(20.385): Jest and Enzyme Documentations

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -19,4 +19,8 @@ describe('<NavigationItems />', () => {
         wrapper.setProps({isAuthenticated: true});
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
+    it('should render a Logout <NavigationItem /> when useer is authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
 });
